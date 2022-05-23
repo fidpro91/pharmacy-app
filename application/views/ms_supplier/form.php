d86edcce33f5803aa67e98c5e29625870eba8f5b
ariefsetyan <ariefsetyanugraha@ymail.com>
2022-05-23 12:09:48 +0700

    ms supplier

--- application/views/ms_supplier/form.php
+++ application/views/ms_supplier/form.php
@@ -1,14 +1,25 @@
     <div class="col-md-12">
-      			<?=form_open("ms_supplier/save",["method"=>"post","class"=>"form-horizontal","id"=>"fm_ms_supplier"],$model)?>
-		<?=form_hidden("supplier_id")?>
-			<?=create_input("supplier_code")?>
-			<?=create_input("supplier_name")?>
-			<?=create_input("supplier_address")?>
-			<?=create_input("supplier_phone")?>
-			<?=create_input("supplier_contact")?>
-			<?=create_input("supplier_active")?>
-			<?=create_input("jenis_supplier")?>
-<?=form_close()?>
+		<?=form_open("ms_supplier/save",["method"=>"post","class"=>"form-vertical","id"=>"fm_ms_supplier"],$model)?>
+		<div class="row">
+			<div class="col-md-6">
+				<?=form_hidden("supplier_id")?>
+				<?=create_input("supplier_code")?>
+				<?=create_input("supplier_name")?>
+				<?=create_textarea("supplier_address")?>
+			</div>
+			<div class="col-md-6">
+
+				<?=create_input("supplier_phone")?>
+				<?=create_input("supplier_contact")?>
+				<?= create_select([
+						"attr" => ["name" => "supplier_active=Status", "id" => "supplier_active", "class" => "form-control", 'required' => true],
+						"option" => [["id" => 't', "text" => "Aktif"], ["id" => 'f', "text" => "Non Aktif"]],
+				]) ?>
+			</div>
+		</div>
+
+
+		<?=form_close()?>
       <div class="box-footer">
       		<button class="btn btn-primary" type="button" onclick="$('#fm_ms_supplier').submit()">Save</button>
       		<button class="btn btn-warning" type="button" id="btn-cancel">Cancel</button>
