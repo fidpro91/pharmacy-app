f903be544de4a2b5ed9d8b4a04d9c0223f1752af
fidpro91 <ufi.alfi@gmail.com>
2022-08-08 11:22:10 +0700

    comodity

--- application/views/ms_comodity/form.php
+++ application/views/ms_comodity/form.php
@@ -1,9 +1,14 @@
     <div class="col-md-12">
-      			<?=form_open("ms_comodity/save",["method"=>"post","class"=>"form-horizontal","id"=>"fm_ms_comodity"],$model)?>
-			<?=create_input("comodity_id")?>
-			<?=create_input("comodity_code")?>
-			<?=create_input("comodity_name")?>
-			<?=create_input("comodity_active")?>
+		<?=form_open("ms_comodity/save",["method"=>"post","class"=>"form-horizontal","id"=>"fm_ms_comodity"],$model)?>
+		<?=form_hidden("comodity_id")?>
+		<?=create_input("comodity_code=Code")?>
+		<?=create_input("comodity_name=Nama")?>
+		<?=create_select2(["attr"=>["name"=>"comodity_active=Status","id"=>"comodity_active","class"=>"form-control"],
+				"option" => [
+						["id" => 't', "text" => "Aktif"],
+						["id" => 'f', "text" => "Tidak Aktif"]
+				],
+		])?>
 <?=form_close()?>
       <div class="box-footer">
       		<button class="btn btn-primary" type="button" onclick="$('#fm_ms_comodity').submit()">Save</button>
