3f223ee1bdd4c8f898a599db215e733068be9c01
fidpro91 <ufi.alfi@gmail.com>
2022-09-28 10:25:13 +0700

    baru

--- application/views/opname_header/index.php
+++ application/views/opname_header/index.php
@@ -1,7 +1,3 @@
-<?php
-print_r($this->session->flashdata('message'));
-die;
-?>
 <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
