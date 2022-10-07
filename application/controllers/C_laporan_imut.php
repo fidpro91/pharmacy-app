df1c9508b0259a772fe8691cad6e556477dad9ed
fidpro91 <ufi.alfi@gmail.com>
2022-10-07 10:32:22 +0700

    laporan imut

--- application/controllers/C_laporan_imut.php
+++ application/controllers/C_laporan_imut.php
@@ -8,7 +8,7 @@ class C_laporan_imut extends MY_Generator
 		$this->datascript
 			->lib_select2()
 			->lib_daterange();
-		$this->load->model('M_laporan_imut');
+		$this->load->model('m_laporan_imut');
 	}
 	public function index()
 	{
@@ -39,11 +39,11 @@ class C_laporan_imut extends MY_Generator
 		$tglPeriodikDari       = date("Y-m-d H:i:s", strtotime($myArray[0]));
 		$tglPeriodikSampai       = date("Y-m-d H:i:s", strtotime($myArray[1]));
 		$data['waktu'] = $tglPeriodikDari."sd".$tglPeriodikSampai;
-		$data['username']       = $this->userData['username'];
+		$data['username']       = $this->session->user_name;
 		$data['judul'] = "Tanggal ".$myArray[0]." Jam 00:00:00 s/d ".$myArray[1]." Jam 23:59:59";
 		$where .= " AND (to_char(s.date_act,'YYYY-MM-DD HH24:MI:SS') between '$tglPeriodikDari' AND '$tglPeriodikSampai')";
 
-		$data['rs']     = $this->m_laporan_imut->get_data_rs($where);
+		$data['rs']     = $this->m_laporan_imut->get_data_rs();
 
 		if ($kepemilikan) {
 			$where .= "and s.own_id = $kepemilikan";
@@ -55,16 +55,16 @@ class C_laporan_imut extends MY_Generator
 		}
 		if ($gol_laporan == '0') {
 			$data['data'] = $this->m_laporan_imut->get_data_lap($where);
-			$this->load->view('farmasi/laporan/v_lap_farmasi_imut',$data);
+			$this->load->view('laporan_imutu/v_lap_farmasi_imut',$data);
 		}elseif($gol_laporan == 1){
 			$data['data'] = $this->m_laporan_imut->get_rekap_dt($where);
-			$this->load->view('farmasi/laporan/v_lap_rekap_imut',$data);
+			$this->load->view('laporan_imutu/v_lap_rekap_imut',$data);
 		}elseif($gol_laporan == 2){
 			$data['data'] = $this->m_laporan_imut->get_data_by_dokter($where);
-			$this->load->view('farmasi/laporan/v_lap_imut_bydokter',$data);
+			$this->load->view('laporan_imutu/v_lap_imut_bydokter',$data);
 		}elseif($gol_laporan == 3){
 			$data['data'] = $this->m_laporan_imut->get_data_by_pasien($where);
-			$this->load->view('farmasi/laporan/v_laporan_terapi_obat',$data);
+			$this->load->view('laporan_imutu/v_laporan_terapi_obat',$data);
 		}
 	}
 }
