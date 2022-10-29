4c97deda7e0b7184a8859906bd4c7bcc4df3964b
root <root@ehos-lapp.1.1.1.1>
2022-10-29 15:34:30 +0700

    baru

--- application/controllers/Recipe.php
+++ application/controllers/Recipe.php
@@ -26,27 +26,152 @@ class Recipe extends MY_Generator {
 	public function save()
 	{
 		$data = $this->input->post();
-		if ($this->m_recipe->validation()) {
-			$input = [];
-			foreach ($this->m_recipe->rules() as $key => $value) {
-				$input[$key] = $data[$key];
-			}
-			if ($data['rcp_id']) {
-				$this->db->where('rcp_id',$data['rcp_id'])->update('newfarmasi.recipe',$input);
-			}else{
-				$this->db->insert('newfarmasi.recipe',$input);
-			}
-			$err = $this->db->error();
-			if ($err['message']) {
-				$this->session->set_flashdata('message','<div class="alert alert-danger alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>'.$err['message'].'</div>');
-			}else{
-				$this->session->set_flashdata('message','<div class="alert alert-success alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>Data berhasil disimpan</div>');
-			}
-		}else{
-			$this->session->set_flashdata('message',validation_errors('<div class="alert alert-danger alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>','</div>'));
-		}
-		redirect('recipe');
+		$totalAll=0;
+		$saleDetailInput=[];
+		$embalaseNonRacikan=0;
+		$saleServices=0;
 
+		//validasi item
+		foreach ($data["list_recipe"] as $x => $value) {
+			if (empty($value["item_id"])) {
+				continue;
+			}
+			$cek = $this->db->query("SELECT s.*,i.item_name FROM newfarmasi.stock s
+         	join admin.ms_item i on s.item_id = i.item_id
+			WHERE s.item_id = ".$value['item_id']."
+			AND own_id = ".$data['own_id']."
+			AND unit_id = ".$data['unit_id'])->row();
+			if ($cek->stock_summary<$value['qty']){
+				echo json_encode([
+					"code" 		=> "204",
+					"message"	=> "Stock item $cek->item_name kurang dari jumlah penjualan",
+				]);
+				exit();
+				break;
+			}
+			$sd = [
+				// "sale_id" 		=> "trim|integer|required",
+				"item_id" 		=> $value["item_id"],
+				"sale_price" 	=> $value["sale_price"]-($value["sale_price"]*$data["percent_profit"]),
+				"sale_qty" 		=> $value["qty"],
+				"racikan" 		=> "f",
+				"dosis" 		=> $value["dosis"],
+				"kronis" 		=> $value["kronis"],
+				"racikan_qty" 	=> 0,
+				// "price_total" 	=> $value["price_total"],
+				"own_id" 		=> $data["own_id"],
+			];
+
+			$saleDetailInput[$x]['percent_profit'] = $data['percent_profit'];
+			if ($value['racikan_id'] != 'null' && $value['racikan_id'] != '') {
+				$saleDetailInput[$x]['racikan_id'] = $value['racikan_id'];
+				$saleDetailInput[$x]['racikan_qty'] = $value['qty'];
+				$saleDetailInput[$x]['racikan_dosis'] = $value['dosis'];
+				$saleDetailInput[$x]['racikan'] = 't';
+				$saleServices += $this->db->get_where("newfarmasi.setting_app",["setting_name"=>"BIAYA RACIKAN"])->row("setting_value");
+			} else {
+				$embalaseNonRacikan += $this->db->get_where("farmasi.ownership",["own_id"=>$data["own_id"]])->row("profit_item");
+			}
+			$saleDetailInput[$x]['subtotal'] = $value['price_total'];
+			$saleDetailInput[$x] = array_merge($sd,$saleDetailInput[$x]);
+			$totalAll += $value["price_total"];
+		}
+		
+		$pasien = $this->db->get_where("yanmed.patient",[
+			"px_id" => $data["px_id"]
+		])->row();
+
+		if ($data["surety_id"] == 1 || $data["surety_id"] == 33) {
+			$sale_type = 0;
+		}else{
+			$sale_type = 1;
+		}
+		$totalAll = $totalAll + $embalaseNonRacikan + $saleServices;
+		$embalase = $totalAll / 100;
+		$embalase = abs(ceil($embalase) - $embalase) * 100;
+		$totalAll = $totalAll + $embalase;
+		$saleInput = [
+			"sale_num" => $this->get_no_sale($data["unit_id"]),
+			"sale_date" => $data["rcp_date"],
+			"unit_id" => $data["unit_id"],
+			"visit_id" => $data["visit_id"],
+			"patient_norm" => $pasien->px_norm,
+			"patient_name" => $pasien->px_name,
+			"kronis" => $saleDetailInput[0]['kronis'],
+			"user_id" => $this->session->user_id,
+			"sale_type" => $sale_type,
+			"sale_status" => "0",
+			"sale_id" => "1",
+			"rcp_id" => $data["rcp_id"],
+			"service_id" => $data["services_id"],
+			"surety_id" => $data["surety_id"],
+			"doctor_id" => $data["par_id"],
+			"own_id" => $data["own_id"],
+			"sale_total" => $totalAll,
+			"embalase_item_sale" => $embalaseNonRacikan,
+			"sale_services" => $saleServices,
+			"unit_id_lay" => $data["unit_id_lay"],
+			"sale_embalase" => $embalase
+		];
+
+		$this->db->trans_begin();
+		$this->db->insert("newfarmasi.sale",$saleInput);
+		// $saleId = $this->db->insert_id();
+		$saleId = 1;
+		$saleDetailInput = array_map(function ($arr) use ($saleId) {
+			return $arr + ['sale_id' => $saleId];
+		}, $saleDetailInput);
+		foreach ($saleDetailInput as $key => $value) {
+			$this->db->insert("newfarmasi.sale_detail",$value);
+			$saleDetailId = $this->db->insert_id();
+			$this->db->where([
+				"item_id" 		=> $value["item_id"],
+				"racikan_id"	=> (isset($value["racikan_id"])?$value["racikan_id"]:null),
+				"rcp_id"		=> $data["rcp_id"]
+			])->update("newfarmasi.recipe_detail",[
+				"saledetail_id" => $saleDetailId
+			]);
+		}
+
+		$this->db->where([
+			"rcp_id"	=> $data["rcp_id"]
+		])->update("newfarmasi.recipe",[
+			"rcp_status" => 1
+		]);
+
+		$err = $this->db->error();
+		if ($err['message']) {
+			$this->db->trans_rollback();
+			$this->session->set_flashdata('message','<div class="alert alert-danger alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>'.$err['message'].'</div>');
+		}else{
+			$this->db->trans_commit();
+			$this->session->set_flashdata('message','<div class="alert alert-success alert-dismissible"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>Data berhasil disimpan</div>');
+		}
+		// redirect('recipe');
+
+	}
+
+	public function get_no_sale($id)
+	{
+		$nickName = $this->db->get_where("admin.ms_unit", ["unit_id" => $id])->row("unit_nickname");
+		
+		$nomor = generate_code_transaksi([
+			// "text"	=> "S/$nickName/NOMOR/" . date("d.m.Y"),
+			"text"	=> "$nickName/NOMOR/" . date("m.Y"),
+			"table"	=> "newfarmasi.nomor_sale",
+			"column"	=> "sale_num",
+			"delimiter" => "/",
+			"number"	=> "2",
+			"lpad"		=> "4",
+			"filter"	=> " AND unit_id = '$id' and date(date_act) = date(now())"
+		]);
+
+		$this->db->insert("newfarmasi.nomor_sale",[
+			"sale_num" 	=> $nomor,
+			"unit_id"	=> $id
+		]);
+
+		return $nomor;
 	}
 
 	public function show_form($id)
@@ -64,15 +189,26 @@ class Recipe extends MY_Generator {
 		$this->load->view("recipe/form", $data);
 	}
 
+	public function show_form_delete($id)
+	{
+		$data["sale"] = $this->db->query("
+		SELECT s.sale_id,s.sale_num,
+		string_agg(concat(mi.item_name,' ',sd.dosis,' [',sd.sale_qty,']'),'<br>')detail_obat
+		FROM newfarmasi.sale s
+		JOIN newfarmasi.sale_detail sd ON s.sale_id = sd.sale_id
+		JOIN admin.ms_item mi on sd.item_id = mi.item_id
+		GROUP BY s.sale_id,s.sale_num")->result();
+		$this->load->view("recipe/v_detail_recipe",$data);
+	}
+
 	public function get_recipe_detail()
 	{
 		$post = $this->input->post();
 		$data = $this->db->query("SELECT rd.*,mi.item_name as label_item_id,racikan_id,racikan_desc,qty,s.stock_summary as stock,
-		(p.price_sell::numeric+(p.price_sell::numeric*so.percent_profit)+ow.profit_item)sale_price
+		(p.price_sell::numeric+(p.price_sell::numeric*so.percent_profit))sale_price
 		FROM newfarmasi.recipe_detail rd
 		JOIN admin.ms_item mi ON mi.item_id = rd.item_id
 		LEFT JOIN newfarmasi.stock s ON s.item_id = rd.item_id AND s.unit_id = ".$post["unit_id"]." AND s.own_id = ".$post["own_id"]."
-		LEFT JOIN farmasi.ownership ow ON ow.own_id = s.own_id
 		LEFT JOIN farmasi.price p ON s.item_id = p.item_id AND s.own_id = p.own_id
 		LEFT JOIN farmasi.surety_ownership so ON so.own_id = s.own_id AND so.surety_id = ".$post["surety_id"]."
 		where rd.rcp_id = ".$post["rcp_id"]."
@@ -189,7 +325,40 @@ class Recipe extends MY_Generator {
             		$obj[] = $row[$value];
             	}
             }
-            $obj[] = create_btnAction(["update","delete"],$row['id_key']);
+            if ($row["rcp_status"] == "0") {
+				$obj[] = create_btnAction([
+					"Checkin" =>
+					[
+						"btn-act" => "set_val('" . $row['id_key'] . "')",
+						"btn-icon" => "fa fa-cart-plus",
+						"btn-class" => "btn-default",
+					]
+				],$row['id_key']);
+			}elseif($row["rcp_status"] == "2") {
+				$obj[] = create_btnAction([
+					"Checkin" =>
+					[
+						"btn-act" => "set_val('" . $row['id_key'] . "')",
+						"btn-icon" => "fa fa-cart-plus",
+						"btn-class" => "btn-default",
+					],
+					"Show" =>
+					[
+						"btn-act" => "deleteRow('" . $row['id_key'] . "')",
+						"btn-icon" => "fa fa-list-alt",
+						"btn-class" => "btn-success",
+					],
+				],$row['id_key']);
+			}elseif ($row["rcp_status"] == "1") {
+				$obj[] = create_btnAction([
+					"Show" =>
+					[
+						"btn-act" => "deleteRow('" . $row['id_key'] . "')",
+						"btn-icon" => "fa fa-list-alt",
+						"btn-class" => "btn-success",
+					]
+				],$row['id_key']);
+			}
             $records["aaData"][] = $obj;
             $no++;
         }
@@ -202,6 +371,9 @@ class Recipe extends MY_Generator {
 	{
 		$data = $this->db->where('rcp_id',$id)
 						 ->join("yanmed.visit v", "v.visit_id=r.visit_id")
+						 ->join("yanmed.services s", "s.srv_id=r.services_id")
+						 ->join("farmasi.surety_ownership so", "so.surety_id=v.surety_id and so.own_id=1")
+						 ->select("*,s.unit_id as unit_id_lay")
 						 ->get("newfarmasi.recipe r")->row();
 
 		echo json_encode($data);
